640ccf7b381971be0851aa1042fef6f005501005
deeksha31oct <deeksha.oct31@gmail.com>
2025-03-30 16:59:04 +0530

    dropdown

--- src/test/java/test/Dropdown.java
+++ src/test/java/test/Dropdown.java
@@ -1,4 +1,8 @@
 package test;
 
 public class Dropdown {
+    public static void main(String []args)
+    {
+
+    }
 }
--- src/test/java/test/WindowActivity.java
+++ src/test/java/test/WindowActivity.java
@@ -1,4 +1,22 @@
 package test;
 
-public class SiblingParetn {
-}
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowActivity
+{
+    public static void main (String[]args) throws InterruptedException {
+        ChromeDriver d = new ChromeDriver();
+        d.get("https://www.google.com/");
+        d.navigate().to("https://rahulshettyacademy.com/AutomationPractice/");
+       Thread.sleep(2000);
+        d.navigate().back();
+        Thread.sleep(2000);
+        d.navigate().forward();
+      //  d.get("https://rahulshettyacademy.com/AutomationPractice/");
+        d.manage().window().maximize();
+        d.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).click();
+        d.findElement(By.xpath("//header/div/button[1]/parent::div/button[2]")).click();
+        d.close();
+
+    }}
